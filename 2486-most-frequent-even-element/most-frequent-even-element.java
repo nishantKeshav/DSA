6af27d60d8655388704c3ec0e2e3a08a366353a7
nishantKeshav <162373669+nishantKeshav@users.noreply.github.com>
2025-04-28 18:51:21 +0530

    Time: 15 ms (84.36%) | Memory: 44.9 MB (77.32%) - LeetSync

--- 2486-most-frequent-even-element/most-frequent-even-element.java
+++ 2486-most-frequent-even-element/most-frequent-even-element.java
@@ -5,8 +5,8 @@ class Solution {
             freq[num]++;
         }
         int count = 0 , ele = -1;
-        for (int i = 0 ; i < freq.length ; i+=1) {
-            if (freq[i] != 0 && i % 2 == 0) {
+        for (int i = 0 ; i < freq.length ; i+=2) {
+            if (freq[i] != 0) {
                 if (freq[i] > count) {
                     count = freq[i];
                     ele = i;
