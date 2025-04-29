38f370f31449fdaa093e0db7bb99fbd34be894ce
nishantKeshav <162373669+nishantKeshav@users.noreply.github.com>
2025-04-29 19:44:35 +0530

    Time: 1 ms (99.58%) | Memory: 42.4 MB (88.67%) - LeetSync

--- 32-longest-valid-parentheses/longest-valid-parentheses.java
+++ 32-longest-valid-parentheses/longest-valid-parentheses.java
@@ -1,22 +1,58 @@
 class Solution {
     public int longestValidParentheses(String s) {
-        Stack<Integer> stack = new Stack<>();
+        // Stack<Integer> stack = new Stack<>();
+        // int n = s.length();
+        // int maxLen = 0;
+        // stack.push(-1);
+        // for (int i = 0 ; i < n ; i++) {
+        //     char ch = s.charAt(i);
+        //     if (ch == '(') {
+        //         stack.push(i);
+        //     } else {
+        //         stack.pop();
+        //         if (stack.isEmpty()) {
+        //             stack.push(i);
+        //         } else {
+        //             maxLen = Math.max(maxLen , i - stack.peek());
+        //         }
+        //     }
+        // }
+        // return maxLen;
+
+        int left  = 0 , right = 0 , max = 0;
         int n = s.length();
-        int maxLen = 0;
-        stack.push(-1);
         for (int i = 0 ; i < n ; i++) {
             char ch = s.charAt(i);
             if (ch == '(') {
-                stack.push(i);
+                left++;
             } else {
-                stack.pop();
-                if (stack.isEmpty()) {
-                    stack.push(i);
-                } else {
-                    maxLen = Math.max(maxLen , i - stack.peek());
-                }
+                right++;
+            }
+            if (left == right) {
+                max = Math.max(max , left + right);
+            }
+            if (right > left) {
+                left = 0;
+                right = 0;
             }
         }
-        return maxLen;
+        left = 0;
+        right = 0;
+        for (int i = n - 1 ; i >= 0 ; i--) {
+            char ch = s.charAt(i);
+            if (ch == ')') {
+                left++;
+            } else {
+                right++;
+            }
+            if (left == right) {
+                max = Math.max(max , left + right);
+            }
+            if (right > left) {
+                left = 0;
+                right = 0;
+            }
+        }
+        return max;
     }
 }
