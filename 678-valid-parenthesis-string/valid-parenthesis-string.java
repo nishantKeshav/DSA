6ed7936ce582163280d29e228c1e6ef0d72620c6
nishantKeshav <162373669+nishantKeshav@users.noreply.github.com>
2025-04-30 11:52:43 +0530

    Time: 0 ms (100.00%) | Memory: 41.3 MB (89.10%) - LeetSync

---
+++ 678-valid-parenthesis-string/valid-parenthesis-string.java
@@ -0,0 +1,45 @@
+class Solution {
+    public boolean checkValidString(String s) {
+        int open = 0 , star = 0;
+        int n = s.length();
+        for (int i = 0 ; i < n ; i++) {
+            char ch = s.charAt(i);
+            if (ch == '(') {
+                open++;
+            } else if (ch == '*') {
+                star++;
+            } else {
+                if (open != 0) {
+                    open--;
+                } else if (star != 0) {
+                    star--;
+                } else {
+                    return false;
+                }
+            }
+        }
+        if (open == 0) {
+            return true;
+        }
+        // System.out.println("check");
+        open = 0;
+        star = 0;
+        for (int i = n - 1 ; i >= 0 ; i--) {
+            char ch = s.charAt(i);
+            if (ch == ')') {
+                open++;
+            } else if (ch == '*') {
+                star++;
+            } else {
+                if (open != 0) {
+                    open--;
+                } else if (star != 0) {
+                    star--;
+                } else {
+                    return false;
+                }
+            }
+        }
+        return true;
+    }
+}
