0a4cee35abc917ac12699ce75a233ab8e391c362
nishantKeshav <162373669+nishantKeshav@users.noreply.github.com>
2025-04-30 13:03:14 +0530

    Time: 20 ms (56.32%) | Memory: 45.8 MB (30.06%) - LeetSync

--- 1371-minimum-remove-to-make-valid-parentheses/minimum-remove-to-make-valid-parentheses.java
+++ 1371-minimum-remove-to-make-valid-parentheses/minimum-remove-to-make-valid-parentheses.java
@@ -1,29 +1,69 @@
 class Solution {
     public String minRemoveToMakeValid(String s) {
         int n = s.length();
-        Stack<Integer> stack = new Stack<>();
-        HashSet<Integer> set = new HashSet<>();
+        int openCount = 0 , closeCount = 0;
         for (int i = 0 ; i < n ; i++) {
             char ch = s.charAt(i);
             if (ch == '(') {
-                stack.add(i);
+                openCount++;
             } else if (ch == ')') {
-                if (!stack.isEmpty()) {
-                    stack.pop();
+                if (openCount > 0) {
+                    openCount--;
                 } else {
-                    set.add(i);
+                    closeCount++;
                 }
             }
         }
-        while(!stack.isEmpty()) {
-            set.add(stack.pop());
-        }
         StringBuilder str = new StringBuilder();
         for (int i = 0 ; i < n ; i++) {
-            if (!set.contains(i)) {
-                str.append(s.charAt(i));
+            char ch = s.charAt(i);
+            if (ch == ')' && closeCount > 0) {
+                closeCount--;
+                continue;
             }
+            str.append(ch);
         }
-        return str.toString();
+        s = str.toString();
+        n = s.length();
+        str.setLength(0);
+        for (int i = n - 1 ; i >= 0 ; i--) {
+            char ch = s.charAt(i);
+            if (ch == '(' && openCount > 0) {
+                openCount--;
+                continue;
+            }
+            str.append(ch);
+        }
+        return str.reverse().toString();
     }
 }
+
+// class Solution {
+//     public String minRemoveToMakeValid(String s) {
+//         int n = s.length();
+//         Stack<Integer> stack = new Stack<>();
+//         HashSet<Integer> set = new HashSet<>();
+//         for (int i = 0 ; i < n ; i++) {
+//             char ch = s.charAt(i);
+//             if (ch == '(') {
+//                 stack.add(i);
+//             } else if (ch == ')') {
+//                 if (!stack.isEmpty()) {
+//                     stack.pop();
+//                 } else {
+//                     set.add(i);
+//                 }
+//             }
+//         }
+//         while(!stack.isEmpty()) {
+//             set.add(stack.pop());
+//         }
+//         StringBuilder str = new StringBuilder();
+//         for (int i = 0 ; i < n ; i++) {
+//             if (!set.contains(i)) {
+//                 str.append(s.charAt(i));
+//             }
+//         }
+//         return str.toString;
+//     }
+// }
