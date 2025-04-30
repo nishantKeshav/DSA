65abf668d6ddd5213b1f7f289e681970dbaf4ccd
nishantKeshav <162373669+nishantKeshav@users.noreply.github.com>
2025-04-30 11:29:00 +0530

    Time: 16 ms (56.91%) | Memory: 45.4 MB (98.68%) - LeetSync

--- 2221-check-if-a-parentheses-string-can-be-valid/check-if-a-parentheses-string-can-be-valid.java
+++ 2221-check-if-a-parentheses-string-can-be-valid/check-if-a-parentheses-string-can-be-valid.java
@@ -4,29 +4,52 @@ class Solution {
         if (n % 2 == 1) {
             return false;
         }
-        Stack<Integer> open = new Stack<Integer>();
-        Stack<Integer> openClose = new Stack<Integer>();
+        // Stack<Integer> open = new Stack<Integer>();
+        // Stack<Integer> openClose = new Stack<Integer>();
+        // for (int i = 0 ; i < n ; i++) {
+        //     char ch = s.charAt(i);
+        //     char locked_ch = locked.charAt(i);
+        //     if (locked_ch == '0') {
+        //         openClose.push(i);
+        //     } else if (ch == '(') {
+        //         open.push(i);
+        //     } else if (ch == ')') {
+        //         if (!open.isEmpty()) {
+        //             open.pop();
+        //         } else if (!openClose.isEmpty()) {
+        //             openClose.pop();
+        //         } else {
+        //             return false;
+        //         }
+        //     }
+        // }
+        // while(!open.isEmpty() && !openClose.empty() && open.peek() < openClose.peek()) {
+        //     open.pop();
+        //     openClose.pop();
+        // }
+        // return open.isEmpty();
+        int open = 0;
         for (int i = 0 ; i < n ; i++) {
-            char ch = s.charAt(i);
-            char locked_ch = locked.charAt(i);
-            if (locked_ch == '0') {
-                openClose.push(i);
-            } else if (ch == '(') {
-                open.push(i);
-            } else if (ch == ')') {
-                if (!open.isEmpty()) {
-                    open.pop();
-                } else if (!openClose.isEmpty()) {
-                    openClose.pop();
-                } else {
-                    return false;
-                }
+            if (s.charAt(i) == '(' || locked.charAt(i) == '0') {
+                open++;
+            } else {
+                open--;
+            }
+            if (open < 0) {
+                return false;
             }
         }
-        while(!open.isEmpty() && !openClose.empty() && open.peek() < openClose.peek()) {
-            open.pop();
-            openClose.pop();
+        open = 0;
+        for (int i = n - 1 ; i >= 0 ; i--) {
+            if (s.charAt(i) == ')' || locked.charAt(i) == '0') {
+                open++;
+            } else {
+                open--;
+            }
+            if (open < 0) {
+                return false;
+            }
         }
-        return open.isEmpty();
+        return true;
     }
 }
