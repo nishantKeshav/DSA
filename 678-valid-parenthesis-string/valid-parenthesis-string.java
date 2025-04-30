3f3948771e405eb008c661a66be3a7760cd9aa2a
nishantKeshav <162373669+nishantKeshav@users.noreply.github.com>
2025-04-30 12:28:39 +0530

    Time: 0 ms (100.00%) | Memory: 41.4 MB (77.69%) - LeetSync

--- 678-valid-parenthesis-string/valid-parenthesis-string.java
+++ 678-valid-parenthesis-string/valid-parenthesis-string.java
@@ -1,49 +1,77 @@
 class Solution {
     public boolean checkValidString(String s) {
-        int open = 0 , star = 0;
+        int min = 0 , max = 0;
         int n = s.length();
         for (int i = 0 ; i < n ; i++) {
             char ch = s.charAt(i);
             if (ch == '(') {
-                open++;
-            } else if (ch == '*') {
-                star++;
-            } else {
-                if (open != 0) {
-                    open--;
-                } else if (star != 0) {
-                    star--;
-                } else {
-                    return false;
+                min++;
+                max++;
+            } else if (ch == ')') {
+                if (min > 0) {
+                    min--;
                 }
+                max--;
+            } else {
+                if (min > 0) {
+                    min--;
+                }
+                max++;
+            }
+            if (max < 0) {
+                return false;
             }
         }
-        if (star < open) {
-            return false;
-        }
-        // System.out.println(open + " " + star);
-        open = 0;
-        star = 0;
-        for (int i = n - 1 ; i >= 0 ; i--) {
-            char ch = s.charAt(i);
-            if (ch == ')') {
-                open++;
-            } else if (ch == '*') {
-                star++;
-            } else {
-                if (open != 0) {
-                    open--;
-                } else if (star != 0) {
-                    star--;
-                } else {
-                    return false;
-                }
-            }
-        }
-        // System.out.println(open + " " + star);
-        if (star < open) {
-            return false;
-        }
-        return true;
+        return min == 0;
     }
 }
+
+// class Solution {
+//     public boolean checkValidString(String s) {
+//         int open = 0 , star = 0;
+//         int n = s.length();
+//         for (int i = 0 ; i < n ; i++) {
+//             char ch = s.charAt(i);
+//             if (ch == '(') {
+//                 open++;
+//             } else if (ch == '*') {
+//                 star++;
+//             } else {
+//                 if (open != 0) {
+//                     open--;
+//                 } else if (star != 0) {
+//                     star--;
+//                 } else {
+//                     return false;
+//                 }
+//             }
+//         }
+//         if (star < open) {
+//             return false;
+//         }
+//         // System.out.println(open + " " + star);
+//         open = 0;
+//         star = 0;
+//         for (int i = n - 1 ; i >= 0 ; i--) {
+//             char ch = s.charAt(i);
+//             if (ch == ')') {
+//                 open++;
+//             } else if (ch == '*') {
+//                 star++;
+//             } else {
+//                 if (open != 0) {
+//                     open--;
+//                 } else if (star != 0) {
+//                     star--;
+//                 } else {
+//                     return false;
+//                 }
+//             }
+//         }
+//         // System.out.println(open + " " + star);
+//         if (star < open) {
+//             return false;
+//         }
+//         return true;
+//     }
+// }
