82c59d218ee6e40cbd8e676126567f1e246e1028
nishantKeshav <162373669+nishantKeshav@users.noreply.github.com>
2025-05-05 17:47:07 +0530

    Time: 7 ms (44.65%) | Memory: 45.8 MB (11.22%) - LeetSync

---
+++ 735-asteroid-collision/asteroid-collision.java
@@ -0,0 +1,71 @@
+class Solution {
+    public int[] asteroidCollision(int[] arr) {
+        int n = arr.length;
+        
+        Stack<Integer> s1 = new Stack<>(); // for +ve
+        Stack<Integer> s2 = new Stack<>(); // for -ve
+        ArrayList<Integer> list = new ArrayList<>();
+        boolean cond = true;
+        for (int i = 0 ; i < n ; i++) {
+            
+            if (arr[i] > 0) {
+                cond = false;
+                s1.push(arr[i]);
+            } else {
+                if (cond == false) {
+                    s2.push(-1 * arr[i]);
+                } else {
+                    list.add(arr[i]);
+                }
+            }
+            if (!s1.isEmpty() && !s2.isEmpty()) {
+                while(!s1.isEmpty() && !s2.isEmpty()) {
+                    if (s2.peek() > s1.peek()) {
+                        s1.pop();
+                    } else if (s1.peek() > s2.peek()) {
+                        s2.pop();
+                    } else {
+                        s1.pop();
+                        s2.pop();
+                    }
+                }
+            }
+            if (s1.isEmpty() && !s2.isEmpty()) {
+                while(!s2.isEmpty()) {
+                    list.add(-1 * s2.pop());
+                }
+            }
+        }
+        // System.out.println(s1);
+        // System.out.println(s2);
+        
+        n = s1.size() + s2.size() + list.size();
+        int idx = 0;
+        int ans[] = new int[n];
+        
+        while(!s1.isEmpty()) {
+            ans[idx++] = s1.pop();
+        }
+        while(!s2.isEmpty()) {
+            ans[idx++] = s2.pop() * -1;
+        }
+        reverse(n, ans);
+        for (int i = 0 ; i < list.size() ; i++) {
+            if (ans[i] == 0) {
+                ans[i] = list.get(i);
+            }
+        }
+        return ans;
+    }
+
+    public void reverse(int n , int arr[]) {
+        int low = 0 , high = n - 1;
+        while(low < high) {
+            int temp = arr[low];
+            arr[low] = arr[high];
+            arr[high] = temp;
+            low++;
+            high--;
+        }
+    }
+}
