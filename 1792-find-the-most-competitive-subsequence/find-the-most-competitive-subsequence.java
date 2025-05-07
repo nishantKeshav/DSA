9eb1fe938e8052785a9446fc9a9c85ec8df78411
nishantKeshav <162373669+nishantKeshav@users.noreply.github.com>
2025-05-07 13:43:22 +0530

    Time: 7 ms (97.97%) | Memory: 61.8 MB (17.44%) - LeetSync

--- 1792-find-the-most-competitive-subsequence/find-the-most-competitive-subsequence.java
+++ 1792-find-the-most-competitive-subsequence/find-the-most-competitive-subsequence.java
@@ -1,22 +1,40 @@
 class Solution {
     public int[] mostCompetitive(int[] nums, int k) {
         int n = nums.length;
-        Stack<Integer> stack = new Stack<>();
-        for (int i = 0 ; i < n ; i++) {
-            while (!stack.isEmpty() && stack.peek() > nums[i] && stack.size() + (n - i - 1) >= k) {
-                stack.pop();
-            }
-            if (stack.size() < k) {
-                stack.push(nums[i]);
-            }
-        }
-        System.out.println(stack);
-        int idx = 0;
         int ans[] = new int[k];
-        while(!stack.isEmpty()) {
-            ans[k - 1 - idx] = stack.pop();
-            idx++;
+        int idx = 0;
+        for (int i = 0 ; i < n ; i++) {
+            while(idx > 0 && ans[idx - 1] > nums[i] && ((idx - 1 + (n - i)) >= k)) {
+                idx--;
+            }
+            if (idx < k) {
+                ans[idx++] = nums[i];
+            }
         }
         return ans;
     }
 }
+
+
+// class Solution {
+//     public int[] mostCompetitive(int[] nums, int k) {
+//         int n = nums.length;
+//         Stack<Integer> stack = new Stack<>();
+//         for (int i = 0 ; i < n ; i++) {
+//             while (!stack.isEmpty() && stack.peek() > nums[i] && stack.size() + (n - i - 1) >= k) {
+//                 stack.pop();
+//             }
+//             if (stack.size() < k) {
+//                 stack.push(nums[i]);
+//             }
+//         }
+//         System.out.println(stack);
+//         int idx = 0;
+//         int ans[] = new int[k];
+//         while(!stack.isEmpty()) {
+//             ans[k - 1 - idx] = stack.pop();
+//             idx++;
+//         }
+//         return ans;
+//     }
+// }
