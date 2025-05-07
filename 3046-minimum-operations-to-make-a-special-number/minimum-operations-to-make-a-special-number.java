6876ecf9364394d62d57aa00888e5af6d4c3813d
nishantKeshav <162373669+nishantKeshav@users.noreply.github.com>
2025-05-07 20:00:56 +0530

    Time: 3 ms (19.05%) | Memory: 42.5 MB (26.67%) - LeetSync

--- 3046-minimum-operations-to-make-a-special-number/minimum-operations-to-make-a-special-number.java
+++ 3046-minimum-operations-to-make-a-special-number/minimum-operations-to-make-a-special-number.java
@@ -1,4 +1,5 @@
 class Solution {
+
     public int minimumOperations(String num) {
         int n = num.length();
         if (num.length() == 1) {
@@ -18,15 +19,15 @@ class Solution {
             }
         }
         int ans = n;
-        for (int i = 0 ; i < n ; i++) {
-            for (int j = i + 1 ; j < n ; j++) {
+        for (int i = 0; i < n; i++) {
+            for (int j = i + 1; j < n; j++) {
                 int val = ((num.charAt(i) - '0') * 10) + (num.charAt(j) - '0');
                 if (val % 25 == 0) {
-                    ans = Math.min(ans , (j - i - 1) + (n - 1 - j));
+                    ans = Math.min(ans, (j - i - 1) + (n - 1 - j));
                 }
-                if (num.charAt(i) == '0') {
-                    ans = Math.min(ans , n - 1);
-                }
+            }
+            if (num.charAt(i) == '0') {
+                ans = Math.min(ans, n - 1);
             }
         }
         return ans;
