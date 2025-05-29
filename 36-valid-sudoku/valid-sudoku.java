d3cb090ba4c905fb91d0a2437b730c122f6ea224
nishantKeshav <162373669+nishantKeshav@users.noreply.github.com>
2025-05-29 19:38:37 +0530

    Time: 20 ms (5.56%) | Memory: 45.1 MB (18.44%) - LeetSync

--- 36-valid-sudoku/valid-sudoku.java
+++ 36-valid-sudoku/valid-sudoku.java
@@ -1,23 +1,26 @@
 class Solution {
     public boolean isValidSudoku(char[][] board) {
-        Set<String> row = new HashSet<>();
-        Set<String> col = new HashSet<>();
-        Set<String> grid = new HashSet<>();
-
+        // Set<String> row = new HashSet<>();
+        // Set<String> col = new HashSet<>();
+        // Set<String> grid = new HashSet<>();
+        Set<String> seen = new HashSet<>();
         for (int i = 0 ; i < 9 ; i++) {
             for (int j = 0 ; j < 9 ; j++) {
                 if (board[i][j] != '.') {
-                    String keyRow = board[i][j] + "-" + i;
-                    String keyCol = board[i][j] + "-" + j;
-                    String keyGrid = board[i][j] + "-" + (i / 3) + "-" + (j / 3);
+                    String keyRow = board[i][j] + "-" + i + "row";
+                    String keyCol = board[i][j] + "-" + j + "col";
+                    String keyGrid = board[i][j] + ":" + (i / 3) + "-" + (j / 3);
 
-                    if (row.contains(keyRow) || col.contains(keyCol) || grid.contains(keyGrid)) {
+                    if (seen.contains(keyRow) || seen.contains(keyCol) || seen.contains(keyGrid)) {
+                        System.out.println(keyRow);
+                        System.out.println(keyCol);
+                        System.out.println(keyGrid);
                         return false;
                     }
 
-                    row.add(keyRow);
-                    col.add(keyCol);
-                    grid.add(keyGrid);
+                    seen.add(keyRow);
+                    seen.add(keyCol);
+                    seen.add(keyGrid);
                 }
             }
         }
