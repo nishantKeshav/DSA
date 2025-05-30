5d23f26712f4bc7114525fa64d9953c23e739710
nishantKeshav <162373669+nishantKeshav@users.noreply.github.com>
2025-05-30 17:43:38 +0530

    Time: 91 ms (47.94%) | Memory: 45.2 MB (19.81%) - LeetSync

--- 494-target-sum/target-sum.java
+++ 494-target-sum/target-sum.java
@@ -1,26 +1,51 @@
+import java.util.*;
+
 class Solution {
     public int findTargetSumWays(int[] nums, int target) {
         int n = nums.length;
-        int dp[][] = new int[n + 1][1000];
-        // for (int arr[] : dp) {
-        //     Arrays.fill(arr , -1);
-        // }
-        return solve(nums , n , 0 , 0 , target , dp);
+        Map<Pair, Integer> dp = new HashMap<>();
+        return solve(nums, n, 0, 0, target, dp);
     }
 
-    public int solve(int nums[] , int n , int idx , int currVal , int target , int dp[][]) {
+    public int solve(int[] nums, int n, int idx, int currVal, int target, Map<Pair, Integer> dp) {
         if (idx == n) {
             return (currVal == target) ? 1 : 0;
         }
 
-        // if (dp[idx][currVal] != -1) {
-        //     return dp[idx][currVal];
-        // }
+        Pair pair = new Pair(idx, currVal);
+        if (dp.containsKey(pair)) {
+            return dp.get(pair);
+        }
 
         int total = 0;
-        total += solve(nums , n , idx + 1 , currVal + nums[idx] , target , dp);
-        total += solve(nums , n , idx + 1 , currVal - nums[idx] , target , dp);
+        total += solve(nums, n, idx + 1, currVal + nums[idx], target, dp);
+        total += solve(nums, n, idx + 1, currVal - nums[idx], target, dp);
+
+        dp.put(pair, total);
 
         return total;
+    }
+}
+
+class Pair {
+    int idx;
+    int currVal;
+
+    Pair(int idx, int currVal) {
+        this.idx = idx;
+        this.currVal = currVal;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (!(o instanceof Pair)) return false;
+        Pair other = (Pair) o;
+        return idx == other.idx && currVal == other.currVal;
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(idx, currVal);
     }
 }
