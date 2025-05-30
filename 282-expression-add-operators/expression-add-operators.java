66cadc5e39c1df38cb260bef0786729ed567fa07
nishantKeshav <162373669+nishantKeshav@users.noreply.github.com>
2025-05-30 12:07:41 +0530

    Time: 55 ms (88.84%) | Memory: 44.9 MB (99.16%) - LeetSync

--- 282-expression-add-operators/expression-add-operators.java
+++ 282-expression-add-operators/expression-add-operators.java
@@ -1,45 +1,45 @@
 class Solution {
+
     public List<String> addOperators(String num, int target) {
         List<String> list = new ArrayList<>();
-        solve(list, 0, 0, 0, num, target, new StringBuilder());
+        solve(list, new StringBuilder(), num, target, 0, 0, 0);
         return list;
     }
 
-    public void solve(List<String> list, int idx, long currSum, long prevNum, String num, int target, StringBuilder str) {
+    public void solve(List<String> list, StringBuilder ans, String num, int target, int idx, long evaluated, long prev) {
         if (idx == num.length()) {
-            if (currSum == target) {
-                list.add(str.toString());
+            if (evaluated == target) {
+                list.add(ans.toString());
             }
             return;
         }
 
+        long currNum = 0;
         for (int i = idx; i < num.length(); i++) {
-            if (i != idx && num.charAt(idx) == '0') break; // prevent leading zero
+            if (i > idx && num.charAt(idx) == '0') {
+                break;
+            }
 
-            String currStr = num.substring(idx, i + 1);
-            long currNum = Long.parseLong(currStr);
-            int len = str.length();
+            currNum = currNum * 10 + (num.charAt(i) - '0');
+
+            int len = ans.length();
 
             if (idx == 0) {
-                // First number, so no operator
-                str.append(currStr);
-                solve(list, i + 1, currNum, currNum, num, target, str);
-                str.setLength(len); // backtrack
+                ans.append(currNum);
+                solve(list, ans, num, target, i + 1, currNum, currNum);
+                ans.setLength(len);
             } else {
-                // '+'
-                str.append("+").append(currStr);
-                solve(list, i + 1, currSum + currNum, currNum, num, target, str);
-                str.setLength(len);
+                ans.append("+").append(currNum);
+                solve(list, ans, num, target, i + 1, evaluated + currNum, currNum);
+                ans.setLength(len);
 
-                // '-'
-                str.append("-").append(currStr);
-                solve(list, i + 1, currSum - currNum, -currNum, num, target, str);
-                str.setLength(len);
+                ans.append("-").append(currNum);
+                solve(list, ans, num, target, i + 1, evaluated - currNum, -currNum);
+                ans.setLength(len);
 
-                // '*'
-                str.append("*").append(currStr);
-                solve(list, i + 1, currSum - prevNum + prevNum * currNum, prevNum * currNum, num, target, str);
-                str.setLength(len);
+                ans.append("*").append(currNum);
+                solve(list, ans, num, target, i + 1, evaluated - prev + prev * currNum, prev * currNum);
+                ans.setLength(len);
             }
         }
     }
