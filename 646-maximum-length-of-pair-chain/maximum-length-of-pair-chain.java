a4925a0b4f8dd72a84f4b87c7bcf956e5ad9bf12
nishantKeshav <162373669+nishantKeshav@users.noreply.github.com>
2025-06-03 17:36:15 +0530

    Time: 189 ms (5.04%) | Memory: 64 MB (5.62%) - LeetSync

---
+++ 646-maximum-length-of-pair-chain/maximum-length-of-pair-chain.java
@@ -0,0 +1,46 @@
+import java.util.Arrays;
+
+class Solution {
+    Integer[][] dp;
+    int OFFSET;
+    int MAX;
+
+    public int findLongestChain(int[][] pairs) {
+        int n = pairs.length;
+        Arrays.sort(pairs, (a, b) -> Integer.compare(a[0], b[0]));
+        int min = Integer.MAX_VALUE;
+        int max = Integer.MIN_VALUE;
+        for (int[] pair : pairs) {
+            min = Math.min(min, pair[0]);
+            max = Math.max(max, pair[1]);
+        }
+        OFFSET = -min;
+        MAX = max + OFFSET + 1;
+        dp = new Integer[n][MAX + 1];
+        return solve(0, pairs, n, Integer.MIN_VALUE);
+    }
+
+    public int solve(int idx, int[][] pairs, int n, int prevLast) {
+        if (idx >= n) {
+            return 0;
+        }
+
+        int offsetIndex = (prevLast == Integer.MIN_VALUE) ? 0 : prevLast + OFFSET;
+        if (dp[idx][offsetIndex] != null) {
+            return dp[idx][offsetIndex];
+        }
+
+        int take = Integer.MIN_VALUE;
+        if (idx == 0) {
+            take = 1 + solve(idx + 1, pairs, n, pairs[idx][1]);
+        } else {
+            if (prevLast < pairs[idx][0]) {
+                take = 1 + solve(idx + 1, pairs, n, pairs[idx][1]);
+            }
+        }
+
+        int not_take = solve(idx + 1, pairs, n, prevLast);
+
+        return dp[idx][offsetIndex] = Math.max(take, not_take);
+    }
+}
